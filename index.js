c7309e005a0a146dd6bd70ac44da34da2509916a
Lassana Magassa <mystuff@lassanamagassa.com>
2018-06-12 02:08:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+
+var a = 5
+
+console.log(a)
+
+function say(greeting, firstName) {
+  return `${greeting}, ${firstName}!`
+}
+
+say('Yo, what up','George')
+
+function add(x, y) {
+  return x * y
+}
+
+add(1, 2)
